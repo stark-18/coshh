8e1aea5008879956dcd3c84cb675f5acabd01d58
Anand Mohan <anand79054sh@gmail.com>
2024-02-16 21:44:40 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='tracking-wide font-semibold w-full bg-[#131833] h-screen'>
+      <div className=''>
+        <p className=' pt-48 mb-5 px-28 text-5xl text-white'>Centre for Open-source</p>
+        <p className='px-28 mb-5 text-5xl text-white'>Software and Hardware</p>
+        </div>
+      <p className='px-28 text-base font-light text-white'>A centre dedicated to open-source software and hardware at the National </p>
+      <p className='px-28 mb-5 text-lgtext-lg font-light text-white'>Institute of Technology Karnataka, Surathkal, Mangalore, India.</p>
+      <p className='px-28 font-bold text-lgtext-lg text-white'>COLLABORATE WITH US:</p>
+      <button className='px-28 text-white border-0'>xyz</button>
+
+    </div>
+  )
+}
+
+export default Home
+
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: 'About' },
+    { id: 3, text: 'Collaborate' },
+    { id: 4, text: 'API day' },
+    { id: 5, text: 'Postman Classroom Program' },
+    { id: 6, text: 'Domains' },
+    { id: 7, text: 'Events' },
+    { id: 8, text: 'Blog' },
+    { id: 9, text: 'Contact' }
+  ];
+
+  return (
+    <div className='bg-[#131833] flex justify-between items-center h-24 w-screen mx-auto px-4 text-white hover:bg-black ease-out duration-300'>
+      {/* Logo */}
+      <h1 className='w-full m-28 text-4xl font-thin text-[#e5eeeb]'>COSH</h1>
+
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 m-1 text-sm cursor-pointer duration-300 hover:text-[#00df9a]'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
